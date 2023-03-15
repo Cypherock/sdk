7df5b85efb78a0de73d60f4707e2d5df3e9bf757
Md Irshad Ansari <irshad@cypherock.com>
2023-03-15 13:07:06 +0530

    fix: prebuild.sh now supports windows

--- packages/app-manager/scripts/prebuild.sh
+++ packages/app-manager/scripts/prebuild.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 rm -rf ./src/proto/generated/*.ts || true
 rm -rf ./src/proto/generated/manager || true
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./src/proto/generated ../../submodules/common/proto/manager/*.proto -I../../submodules/common/proto
+
+if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+  protoc --plugin=protoc-gen-ts_proto=$(pwd)/node_modules/.bin/protoc-gen-ts_proto.cmd  --ts_proto_out=./src/proto/generated ../../submodules/common/proto/manager/*.proto -I../../submodules/common/proto
+else
+  protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./src/proto/generated ../../submodules/common/proto/manager/*.proto -I../../submodules/common/proto
+fi
--- packages/core/scripts/prebuild.sh
+++ packages/core/scripts/prebuild.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./src/encoders/proto/generated ../../submodules/common/proto/*.proto -I../../submodules/common/proto
+if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+  protoc --plugin=protoc-gen-ts_proto=$(pwd)/node_modules/.bin/protoc-gen-ts_proto.cmd --ts_proto_out=./src/encoders/proto/generated ../../submodules/common/proto/*.proto -I../../submodules/common/proto
+else
+  protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./src/encoders/proto/generated ../../submodules/common/proto/*.proto -I../../submodules/common/proto
+fi
