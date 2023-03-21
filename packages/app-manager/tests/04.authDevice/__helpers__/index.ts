b6a25a7fb87f5b13867c518a2d28718fd22a71a8
Md Irshad Ansari <irshad@cypherock.com>
2023-03-21 16:50:51 +0530

    refactor: manager app test mocks

--- packages/app-manager/tests/04.authDevice/__helpers__/index.ts
+++ packages/app-manager/tests/04.authDevice/__helpers__/index.ts
@@ -1,51 +1,13 @@
-import { expect, jest } from '@jest/globals';
+import { expect } from '@jest/globals';
 
-import * as sdkMocks from '../../../src/__mocks__/sdk';
+import * as superMocks from '../../__helpers__';
 import * as verificationServiceMocks from '../../../src/__mocks__/authVerificationService';
 import { getDeviceInfo as getDeviceInfoMock } from '../../../src/__mocks__/getDeviceInfo';
 
 import { IAuthDeviceTestCase } from '../__fixtures__';
 
-const onEvent = jest.fn();
-
 export function setupMocks(testCase: IAuthDeviceTestCase) {
-  testCase.queries.forEach(() => {
-    sdkMocks.sendQuery.mockReturnValueOnce(Promise.resolve(undefined));
-  });
-
-  testCase.results.forEach(result => {
-    /*
-     * 1. Simulate calling onStatus event for waitForResult
-     * 2. Expect the onEvent to be called as described in the testCase
-     */
-    sdkMocks.waitForResult.mockImplementationOnce(async params => {
-      if (params?.onStatus && result.statuses) {
-        let onEventCalls = 0;
-
-        for (const status of result.statuses) {
-          params.onStatus({ flowStatus: status.flowStatus } as any);
-
-          if (status.expectEventCalls !== undefined) {
-            for (let i = 0; i < status.expectEventCalls.length; i += 1) {
-              const mockIndex =
-                onEvent.mock.calls.length - status.expectEventCalls.length + i;
-
-              expect(onEvent.mock.calls[mockIndex]).toEqual([
-                status.expectEventCalls[i],
-              ]);
-            }
-
-            onEventCalls += status.expectEventCalls.length;
-            expect(onEvent).toHaveBeenCalledTimes(onEventCalls);
-          } else {
-            expect(onEvent).toHaveBeenCalledTimes(onEventCalls);
-          }
-        }
-      }
-
-      return result.data;
-    });
-  });
+  const onEvent = superMocks.setupMocks(testCase);
 
   verificationServiceMocks.verifySerialSignature.mockReturnValueOnce(
     testCase.mocks.challenge,
@@ -59,14 +21,7 @@ export function setupMocks(testCase: IAuthDeviceTestCase) {
 }
 
 export function clearMocks() {
-  onEvent.mockClear();
-
-  sdkMocks.create.mockClear();
-
-  sdkMocks.sendQuery.mockReset();
-  sdkMocks.waitForResult.mockReset();
-
-  sdkMocks.runOperation.mockClear();
+  superMocks.clearMocks();
 
   verificationServiceMocks.verifySerialSignature.mockReset();
   verificationServiceMocks.verifyChallengeSignature.mockReset();
@@ -75,11 +30,7 @@ export function clearMocks() {
 }
 
 export function expectMockCalls(testCase: IAuthDeviceTestCase) {
-  expect(sdkMocks.runOperation).toHaveBeenCalledTimes(1);
-  expect(sdkMocks.sendQuery.mock.calls.map(elem => elem[0])).toEqual(
-    testCase.queries.map(elem => elem.data),
-  );
-  expect(onEvent.mock.calls).toEqual(testCase.mocks.eventCalls);
+  superMocks.expectMockCalls(testCase);
 
   if (testCase.mocks.verifyChallengeSignatureCalls) {
     expect(
--- packages/app-manager/tests/05.authCard/__helpers__/index.ts
+++ packages/app-manager/tests/05.authCard/__helpers__/index.ts
@@ -1,50 +1,12 @@
-import { expect, jest } from '@jest/globals';
+import { expect } from '@jest/globals';
 
-import * as sdkMocks from '../../../src/__mocks__/sdk';
+import * as superMocks from '../../__helpers__';
 import * as verificationServiceMocks from '../../../src/__mocks__/authVerificationService';
 
 import { IAuthCardTestCase } from '../__fixtures__';
 
-const onEvent = jest.fn();
-
 export function setupMocks(testCase: IAuthCardTestCase) {
-  testCase.queries.forEach(() => {
-    sdkMocks.sendQuery.mockReturnValueOnce(Promise.resolve(undefined));
-  });
-
-  testCase.results.forEach(result => {
-    /*
-     * 1. Simulate calling onStatus event for waitForResult
-     * 2. Expect the onEvent to be called as described in the testCase
-     */
-    sdkMocks.waitForResult.mockImplementationOnce(async params => {
-      if (params?.onStatus && result.statuses) {
-        let onEventCalls = 0;
-
-        for (const status of result.statuses) {
-          params.onStatus({ flowStatus: status.flowStatus } as any);
-
-          if (status.expectEventCalls !== undefined) {
-            for (let i = 0; i < status.expectEventCalls.length; i += 1) {
-              const mockIndex =
-                onEvent.mock.calls.length - status.expectEventCalls.length + i;
-
-              expect(onEvent.mock.calls[mockIndex]).toEqual([
-                status.expectEventCalls[i],
-              ]);
-            }
-
-            onEventCalls += status.expectEventCalls.length;
-            expect(onEvent).toHaveBeenCalledTimes(onEventCalls);
-          } else {
-            expect(onEvent).toHaveBeenCalledTimes(onEventCalls);
-          }
-        }
-      }
-
-      return result.data;
-    });
-  });
+  const onEvent = superMocks.setupMocks(testCase);
 
   if (testCase.mocks) {
     verificationServiceMocks.verifySerialSignature.mockReturnValueOnce(
@@ -59,30 +21,16 @@ export function setupMocks(testCase: IAuthCardTestCase) {
 }
 
 export function clearMocks() {
-  onEvent.mockClear();
-
-  sdkMocks.create.mockClear();
-
-  sdkMocks.sendQuery.mockReset();
-  sdkMocks.waitForResult.mockReset();
-
-  sdkMocks.runOperation.mockClear();
+  superMocks.clearMocks();
 
   verificationServiceMocks.verifySerialSignature.mockReset();
   verificationServiceMocks.verifyChallengeSignature.mockReset();
 }
 
 export function expectMockCalls(testCase: IAuthCardTestCase) {
-  expect(sdkMocks.runOperation).toHaveBeenCalledTimes(1);
-  expect(sdkMocks.sendQuery.mock.calls.map(elem => elem[0])).toEqual(
-    testCase.queries.map(elem => elem.data),
-  );
+  superMocks.expectMockCalls(testCase);
 
   if (!testCase.mocks) return;
-
-  if (testCase.mocks.eventCalls) {
-    expect(onEvent.mock.calls).toEqual(testCase.mocks.eventCalls);
-  }
 
   if (testCase.mocks.verifyChallengeSignatureCalls) {
     expect(
