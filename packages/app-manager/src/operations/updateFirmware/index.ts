faa79317aa5629d33aff8b2bf84ef33ececefb66
Md Irshad Ansari <irshadjsr21@gmail.com>
2023-06-21 22:56:54 +0530

    feat: added error cases for device update

--- packages/app-manager/src/operations/updateFirmware/index.ts
+++ packages/app-manager/src/operations/updateFirmware/index.ts
@@ -4,18 +4,39 @@ import {
   stringToVersion,
   uint8ArrayToHex,
 } from '@cypherock/sdk-utils';
+import {
+  FirmwareUpdateError,
+  IFirmwareUpdateErrorResponse,
+} from '../../proto/generated/types';
 import { firmwareService } from '../../services';
 import {
   assertOrThrowInvalidResult,
   OperationHelper,
   logger as rootLogger,
 } from '../../utils';
+import { UpdateFirmwareError, UpdateFirmwareErrorType } from './error';
 import { createBootloaderSDK, waitForReconnection } from './helpers';
 import { IUpdateFirmwareParams } from './types';
 
 export * from './types';
 
 const logger = createLoggerWithPrefix(rootLogger, 'UpdateFirmware');
+
+const parseFirmwareUpdateError = async (
+  error?: IFirmwareUpdateErrorResponse,
+) => {
+  if (!error) return;
+
+  const errorTypesMap: Record<FirmwareUpdateError, UpdateFirmwareErrorType> = {
+    [FirmwareUpdateError.UNRECOGNIZED]: UpdateFirmwareErrorType.UNKNOWN_ERROR,
+    [FirmwareUpdateError.FIRMWARE_UPDATE_ERROR_UNKNOWN]:
+      UpdateFirmwareErrorType.UNKNOWN_ERROR,
+    [FirmwareUpdateError.FIRMWARE_UPDATE_ERROR_VERSION_NOT_ALLOWED]:
+      UpdateFirmwareErrorType.VERSION_NOT_ALLOWED,
+  };
+
+  throw new UpdateFirmwareError(errorTypesMap[error.error]);
+};
 
 export const updateFirmware = async (
   sdk: ISDK,
@@ -42,6 +63,7 @@ export const updateFirmware = async (
   if (!(await sdk.isInBootloader())) {
     await helper.sendQuery({ initiate: { version } });
     const result = await helper.waitForResult();
+    parseFirmwareUpdateError(result.error);
     logger.verbose('FirmwareUpdateConfirmedResponse', { result });
     assertOrThrowInvalidResult(result.confirmed);
   }
