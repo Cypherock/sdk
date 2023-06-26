7486785f22f9ba214ca94e6e97e80e81e79c38fe
Md Irshad Ansari <irshadjsr21@gmail.com>
2023-06-26 17:24:42 +0530

    refactor: added firmware update to nodejs app

--- apps/node/src/index.ts
+++ apps/node/src/index.ts
@@ -1,4 +1,5 @@
 import DeviceConnection from '@cypherock/sdk-hw-hid';
+// import DeviceConnectionSerialport from '@cypherock/sdk-hw-serialport';
 import { ManagerApp } from '@cypherock/sdk-app-manager';
 
 const run = async () => {
@@ -8,13 +9,18 @@ const run = async () => {
 
   const deviceInfo = await managerApp.getDeviceInfo();
 
+  console.log(deviceInfo);
+
   await managerApp.authDevice();
 
   await managerApp.trainCard({ onWallets: async () => true });
 
   await managerApp.authCard();
 
-  console.log(deviceInfo);
+  // await managerApp.updateFirmware({
+  //   createConnection: async () => DeviceConnection.create(),
+  //   createSerialportConnection: async () => DeviceConnectionSerialport.create(),
+  // });
 };
 
 run();
