0d22424ca46c71bf505766be79ea34e6305bee56
TejasvCypherock <107414753+TejasvCypherock@users.noreply.github.com>
2023-07-28 15:22:08 +0530

    refactor: export bitcoinjs-lib type (#47)
    * refactor: export btc js lib type
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: Tejasv Sharma <tejasvonly@gmail.com>

--- packages/app-btc/src/index.ts
+++ packages/app-btc/src/index.ts
@@ -2,3 +2,4 @@ export * from './app';
 export * from './proto/generated/types';
 export * from './operations/types';
 export { updateLogger, setBitcoinJSLib } from './utils';
+export type { bitcoinJsLibType } from './utils';
