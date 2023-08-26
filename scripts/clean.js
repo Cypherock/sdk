ab4bdb8940255539daf93e6405c1af0164e06776
Md Irshad Ansari <irshadjsr21@gmail.com>
2023-08-26 15:57:12 +0530

    chore: updated clean script

--- scripts/clean.js
+++ scripts/clean.js
@@ -10,6 +10,7 @@ const packages = {
   'packages/app-btc': [...commonTSFolders, 'src/proto/generated'],
   'packages/app-evm': [...commonTSFolders, 'src/proto/generated'],
   'packages/app-manager': [...commonTSFolders, 'src/proto/generated'],
+  'packages/app-near': [...commonTSFolders, 'src/proto/generated'],
   'packages/core': [...commonTSFolders, 'src/encoders/proto/generated'],
   'packages/hw-hid': [...commonTSFolders],
   'packages/hw-serialport': [...commonTSFolders],
