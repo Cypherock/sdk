2837702814da2251397714f763b6332ba7523eb6
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2024-03-04 16:59:41 +0530

    chore: temp

--- apps/browser/src/services/ethereum.tsx
+++ apps/browser/src/services/ethereum.tsx
@@ -1,11 +1,10 @@
 import { EvmApp } from "@cypherock/sdk-app-evm";
 import { IWalletItem } from "@cypherock/sdk-app-manager";
 import DeviceConnection from "@cypherock/sdk-hw-webusb";
-import assert from "assert";
 import { useEffect, useState } from "react";
 import { populateWalletSelection } from "./wallet";
 import { ethers } from 'ethers';
-import { setEthersLib } from '@cypherock/sdk-app-evm';
+import { setEthersLib, IGetPublicKeysResult } from '@cypherock/sdk-app-evm';
 
 type NetworkDetails = {
   name: string;
@@ -21,7 +20,7 @@ type AccountType = {
 type ServiceDetails = {
   name: string | undefined;
   chainId: number | undefined;
-  address: string | undefined;
+  accounts: IGetPublicKeysResult | undefined;
   wallet: IWalletItem | undefined;
   accountNumber: number | undefined;
   accountType: AccountType | undefined;
@@ -78,7 +77,7 @@ export const EthereumService: React.FC<{ devConnection: DeviceConnection, wallet
   const [action, setAction] = useState<string | undefined>(undefined);
   const [appError, setAppError] = useState<string | undefined>(undefined);
   const [appResult, setAppResult] = useState<string | undefined>(undefined);
-  const [serviceDetails, setServiceDetails] = useState<ServiceDetails>({name: undefined, chainId: undefined, address: undefined, wallet: undefined, accountNumber: undefined, accountType: undefined, derivationPath: undefined});
+  const [serviceDetails, setServiceDetails] = useState<ServiceDetails>({name: undefined, chainId: undefined, accounts: undefined, wallet: undefined, accountNumber: 0, accountType: undefined, derivationPath: undefined});
   const [accountReady, setAccountReady] = useState<boolean>(false);
 
   const init = async (connection: DeviceConnection) => {
@@ -139,7 +138,9 @@ export const EthereumService: React.FC<{ devConnection: DeviceConnection, wallet
         case "1":
           const path = [...serviceDetails.accountType?.derivationPath];
           path[serviceDetails.accountType.varIdx] += serviceDetails.accountNumber;
-          return await instance?.getPublicKeys({chainId: 1, walletId: serviceDetails.wallet.id, derivationPaths: [{path}]});
+          const accounts = await instance?.getPublicKeys({chainId: 1, walletId: serviceDetails.wallet.id, derivationPaths: [{path}]});
+          setServiceDetails({...serviceDetails, accounts})
+          return accounts;
   
         default:
           return "Invalid action or not implemented";
@@ -185,7 +186,11 @@ export const EthereumService: React.FC<{ devConnection: DeviceConnection, wallet
 
   const handleAccTypeSelection = (indexValue: string) => {
     const idx = Number(indexValue);
-    setServiceDetails({...serviceDetails, accountType: (typeof idx === 'number' && idx < networks.length ? accountTypes[idx] : undefined)})
+    const accountType = (typeof idx === 'number' && idx < networks.length ? accountTypes[idx] : undefined);
+    const derivationPath = accountType ? [...accountType.derivationPath] : undefined;
+    if (derivationPath && accountType)
+      derivationPath[accountType.varIdx] += serviceDetails.accountNumber ? serviceDetails.accountNumber : 0;
+    setServiceDetails({...serviceDetails, derivationPath, accountType});
   };
 
   const handleNumberSelection = (indexValue: string) => {
@@ -195,6 +200,238 @@ export const EthereumService: React.FC<{ devConnection: DeviceConnection, wallet
       derivationPath[serviceDetails.accountType.varIdx] += idx;
     setServiceDetails({...serviceDetails, accountNumber: (typeof idx === 'number' && idx >= 0 && idx < 0x80000000 ? idx : undefined), derivationPath})
   };
+
+  const submitFormButton = async () => {
+    let params;
+    if (type.value === '0x0') {
+      params =
+        {
+          from: serviceDetails.accounts?.addresses[0],
+          to: toDiv.value,
+          value: amount.value,
+          gasPrice: gasPrice.value,
+          type: type.value,
+          data: src_data.value,
+        };
+    } else {
+      params =
+        {
+          from: serviceDetails.accounts?.addresses[0],
+          to: toDiv.value,
+          value: amount.value,
+          maxFeePerGas: maxFee.value,
+          maxPriorityFeePerGas: maxPriority.value,
+          type: type.value,
+          data: src_data.value,
+        };
+    }
+    console.log({params});
+    const result = await ethers.Transaction.from(params);
+    console.log({result});
+  };
+
+  /**
+   * eth_sign
+   */
+  const ethSign = async () => {
+    try {
+      // const msg = 'Sample message to hash for signature'
+      // const msgHash = keccak256(msg)
+      const msg =
+        '0x879a053d4800c6354e76c7985a865d2922c82fb5b3f4577b2fe08b998954f2e0';
+      const ethResult = await src_provider.request({
+        method: 'eth_sign',
+        params: [serviceDetails.accounts?.addresses[0], msg],
+      });
+      ethSignResult.innerHTML = JSON.stringify(ethResult);
+    } catch (err) {
+      console.error(err);
+      setAppError(`Error: ${(err as any).message}`);
+    }
+  };
+
+  /**
+   * Personal Sign
+   */
+  const personalSign = async () => {
+    const exampleMessage = 'Example `personal_sign` message';
+    try {
+      const from = serviceDetails.accounts?.addresses[0];
+      const msg = `0x${src_Buffer.from(exampleMessage, 'utf8').toString('hex')}`;
+      const sign = await src_provider.request({
+        method: 'personal_sign',
+        params: [msg, from, 'Example password'],
+      });
+      personalSignResult.innerHTML = sign;
+      personalSignVerify.disabled = false;
+    } catch (err) {
+      console.error(err);
+      setAppError(`Error: ${(err as any).message}`);
+    }
+  };
+
+  /**
+   * Sign In With Ethereum helper
+   */
+  const siweSign = async (siweMessage) => {
+    try {
+      const from = serviceDetails.accounts?.addresses[0];
+      const msg = `0x${src_Buffer.from(siweMessage, 'utf8').toString('hex')}`;
+      const sign = await src_provider.request({
+        method: 'personal_sign',
+        params: [msg, from, 'Example password'],
+      });
+      siweResult.innerHTML = sign;
+    } catch (err) {
+      console.error(err);
+      setAppError(`Error: ${(err as any).message}`);
+    }
+  };
+
+  /**
+   * Sign In With Ethereum
+   */
+  const siwe = async () => {
+    const domain = window.location.host;
+    const from = serviceDetails.accounts?.addresses[0];
+    const siweMessage = `${domain} wants you to sign in with your Ethereum account:\
+    ${from}\
+    \
+    I accept the MetaMask Terms of Service: https://community.metamask.io/tos\
+    \
+    URI: https://${domain}\
+    Version: 1\
+    Chain ID: 1\
+    Nonce: 32891757\
+    Issued At: 2021-09-30T16:25:24.000Z`;
+    siweSign(siweMessage);
+  };
+
+  /**
+   * Sign In With Ethereum (with Resources)
+   */
+  const siweResources = async () => {
+    const domain = window.location.host;
+    const from = serviceDetails.accounts?.addresses[0];
+    const siweMessageResources = `${domain} wants you to sign in with your Ethereum account:\
+    ${from}\
+    \
+    I accept the MetaMask Terms of Service: https://community.metamask.io/tos\
+    \
+    URI: https://${domain}\
+    Version: 1\
+    Chain ID: 1\
+    Nonce: 32891757\
+    Issued At: 2021-09-30T16:25:24.000Z\
+    Not Before: 2022-03-17T12:45:13.610Z\
+    Request ID: some_id\
+    Resources:\
+    - ipfs://Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu\
+    - https://example.com/my-web2-claim.json`;
+    siweSign(siweMessageResources);
+  };
+
+  /**
+   * Sign In With Ethereum (Bad Domain)
+   */
+  const siweBadDomain = async () => {
+    const domain = 'metamask.badactor.io';
+    const from = serviceDetails.accounts?.addresses[0];
+    const siweMessageBadDomain = `${domain} wants you to sign in with your Ethereum account:\
+    ${from}\
+    \
+    I accept the MetaMask Terms of Service: https://community.metamask.io/tos\
+    \
+    URI: https://${domain}\
+    Version: 1\
+    Chain ID: 1\
+    Nonce: 32891757\
+    Issued At: 2021-09-30T16:25:24.000Z\
+    Resources:\
+    - ipfs://Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu\
+    - https://example.com/my-web2-claim.json`;
+    siweSign(siweMessageBadDomain);
+  };
+
+  /**
+   * Sign In With Ethereum (Bad Account)
+   */
+  const siweBadAccount = async () => {
+    const domain = window.location.host;
+    const from = '0x0000000000000000000000000000000000000000';
+    const siweMessageBadAccount = `${domain} wants you to sign in with your Ethereum account:\
+    ${from}\
+    \
+    I accept the MetaMask Terms of Service: https://community.metamask.io/tos\
+    \
+    URI: https://${domain}\
+    Version: 1\
+    Chain ID: 1\
+    Nonce: 32891757\
+    Issued At: 2021-09-30T16:25:24.000Z\
+    Resources:\
+    - ipfs://Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu\
+    - https://example.com/my-web2-claim.json`;
+    siweSign(siweMessageBadAccount);
+  };
+
+  /**
+   * Sign In With Ethereum (Malformed)
+   */
+  const siweMalformed = async () => {
+    const domain = window.location.host;
+    const from = serviceDetails.accounts?.addresses[0];
+    const siweMessageMissing = `${domain} wants you to sign in with your Ethereum account:\
+    ${from}\
+    \
+    I accept the MetaMask Terms of Service: https://community.metamask.io/tos\
+    \
+    Version: 1\
+    Nonce: 32891757\
+    Issued At: 2021-09-30T16:25:24Z`;
+    siweSign(siweMessageMissing);
+  };
+
+  /**
+   * Personal Sign Verify
+   */
+  const personalSignVerify = async () => {
+    const exampleMessage = 'Example `personal_sign` message';
+    try {
+      const from = serviceDetails.accounts?.addresses[0];
+      const msg = `0x${src_Buffer.from(exampleMessage, 'utf8').toString('hex')}`;
+      const sign = personalSignResult.innerHTML;
+      const recoveredAddr = (0,eth_sig_util.recoverPersonalSignature)({
+        data: msg,
+        sig: sign,
+      });
+      if (recoveredAddr === from) {
+        console.log(`SigUtil Successfully verified signer as ${recoveredAddr}`);
+        personalSignVerifySigUtilResult.innerHTML = recoveredAddr;
+      } else {
+        console.log(
+          `SigUtil Failed to verify signer when comparing ${recoveredAddr} to ${from}`,
+        );
+        console.log(`Failed comparing ${recoveredAddr} to ${from}`);
+      }
+      const ecRecoverAddr = await src_provider.request({
+        method: 'personal_ecRecover',
+        params: [msg, sign],
+      });
+      if (ecRecoverAddr === from) {
+        console.log(`Successfully ecRecovered signer as ${ecRecoverAddr}`);
+        personalSignVerifyECRecoverResult.innerHTML = ecRecoverAddr;
+      } else {
+        console.log(
+          `Failed to verify signer when comparing ${ecRecoverAddr} to ${from}`,
+        );
+      }
+    } catch (err) {
+      console.error(err);
+      setAppError(`Error: ${(err as any).message}`);
+      setAppError(`Error: ${(err as any).message}`);
+    }
+  }
 
   useEffect(() => {
     init(connection);
@@ -239,7 +476,7 @@ export const EthereumService: React.FC<{ devConnection: DeviceConnection, wallet
               </div>
               <div className="form-group">
                 <label>Account Number</label><br/>
-                <input disabled={serviceDetails.accountType === undefined} className="form-control" min={0} max={0x7fffffff} type="number" placeholder="0" onChange={(evt) => handleNumberSelection(evt.target.value)} />
+                <input disabled={serviceDetails.accountType === undefined} className="form-control" min={0} max={0x7fffffff} type="number" placeholder="0" defaultValue={0} onChange={(evt) => handleNumberSelection(evt.target.value)} />
               </div>
             </div>
             <div className="col-xl-4 col-lg-6 col-md-12 col-sm-12 col-12">
@@ -259,11 +496,11 @@ export const EthereumService: React.FC<{ devConnection: DeviceConnection, wallet
             <div className="col-xl-4 col-lg-6 col-md-12 col-sm-12 col-12">
               <div className="form-group">
                 <label>Account Address</label>
-                <input className="form-control" id="accountAddress" value={`${""}`} disabled={true} />
+                <input className="form-control" id="accountAddress" value={`${serviceDetails.accounts?.addresses[0]}`} disabled={true} />
               </div>
               <div className="form-group">
                 <label>Public key</label>
-                <input className="form-control" id="publicKey" value={`${""}`} disabled={true} />
+                <input className="form-control" id="publicKey" value={`${serviceDetails.accounts?.publicKeys[0]}`} disabled={true} />
               </div>
               <button disabled={!accountReady} onClick={async () => setAppResult(JSON.stringify(await doAction('1'), null, 2))} className="btn btn-primary btn-lg btn-block mb-3">GET ADDRESS</button>
             </div>
@@ -280,7 +517,7 @@ export const EthereumService: React.FC<{ devConnection: DeviceConnection, wallet
                     </h4>
                     <div className="form-group">
                       <label>From</label>
-                      <input className="form-control" type="text" id="fromInput" />
+                      <input disabled className="form-control" type="text" id="fromInput" value={`${serviceDetails.accounts?.addresses[0]}`}/>
                     </div>
                     <div className="form-group">
                       <label>To</label>
