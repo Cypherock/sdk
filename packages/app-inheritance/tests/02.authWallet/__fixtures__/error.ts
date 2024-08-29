5a70eaca563c1c716b421fe1b40cc5b41dd1a9be
Vaibhav Sethia <vrockz747@gmail.com>
2024-08-29 15:30:30 +0530

    feat: add seed based wallet auth (#121)
    * feat: add seed based wallet auth
    
    * feat: update wallet auth flow for seed based auth
    
    * chore: update sumbmodule
    
    ---------
    
    Co-authored-by: TejasvOnly <tejasvonly@gmail.com>

--- packages/app-inheritance/tests/02.authWallet/__fixtures__/error.ts
+++ packages/app-inheritance/tests/02.authWallet/__fixtures__/error.ts
@@ -3,11 +3,12 @@ import {
   DeviceAppErrorType,
   deviceAppErrorTypeDetails,
 } from '@cypherock/sdk-interfaces';
+import { IAuthWalletParams } from '../../../src';
 import { Query } from '../../../src/proto/generated/inheritance/core';
 import { IAuthWalletTestCase } from './types';
 
 const commonParams = {
-  params: {
+  params: <IAuthWalletParams>{
     walletId: new Uint8Array([
       199, 89, 252, 26, 32, 135, 183, 211, 90, 220, 38, 17, 160, 103, 233, 62,
       110, 172, 92, 20, 35, 250, 190, 146, 62, 8, 53, 86, 128, 26, 3, 187,
@@ -17,7 +18,8 @@ const commonParams = {
       110, 172, 92, 20, 35, 250, 190, 146, 62, 8, 53, 86, 128, 26, 3, 187, 121,
       64,
     ]),
-    isPublicKey: true,
+    withPublicKey: true,
+    type: 'wallet-based',
   },
   queries: [
     {
@@ -37,7 +39,9 @@ const commonParams = {
                   103, 233, 62, 110, 172, 92, 20, 35, 250, 190, 146, 62, 8, 53,
                   86, 128, 26, 3, 187, 121, 64,
                 ]),
-                isPublicKey: true,
+                withPublicKey: true,
+                doSeedBased: false,
+                doWalletBased: true,
               },
             },
           }),
