9f46e05e184eed959b265cd0cab9523f652e0a41
TejasvOnly <tejasvonly@gmail.com>
2024-09-20 06:47:42 -0700

    chore: rebase and update submodule

--- apps/node/src/index.ts
+++ apps/node/src/index.ts
@@ -1,5 +1,5 @@
 import DeviceConnection, {
-  updateLogger as updateLoggerHid,
+    updateLogger as updateLoggerHid,
 } from '@cypherock/sdk-hw-hid';
 import DeviceConnectionSerialport from '@cypherock/sdk-hw-serialport';
 import { ManagerApp, updateLogger } from '@cypherock/sdk-app-manager';
@@ -17,72 +17,67 @@ import { createServiceLogger } from './logger';
 import { InheritanceApp } from '@cypherock/sdk-app-inheritance';
 
 const run = async () => {
-  updateLogger(createServiceLogger);
-  updateLoggerCore(createServiceLogger);
-  updateLoggerHid(createServiceLogger);
+    updateLogger(createServiceLogger);
+    updateLoggerCore(createServiceLogger);
+    updateLoggerHid(createServiceLogger);
 
-  setBitcoinJSLib(bitcoinJsLib);
-  setEthersLib(ethers);
-  setNearApiJs(nearApiJs);
-  setSolanaWeb3(solanaWeb3);
+    setBitcoinJSLib(bitcoinJsLib);
+    setEthersLib(ethers);
+    setNearApiJs(nearApiJs);
+    setSolanaWeb3(solanaWeb3);
 
-  let connection: IDeviceConnection;
+    let connection: IDeviceConnection;
 
-  try {
-    connection = await DeviceConnection.create();
-  } catch (error) {
-    connection = await DeviceConnectionSerialport.create();
-  }
+    try {
+        connection = await DeviceConnection.create();
+    } catch (error) {
+        connection = await DeviceConnectionSerialport.create();
+    }
 
-  console.log('started');
+    console.log('started');
 
-  const managerApp = await ManagerApp.create(connection);
+    const managerApp = await ManagerApp.create(connection);
 
-  const { walletList } = await managerApp.getWallets();
+    const { walletList } = await managerApp.getWallets();
 
-  const inheritanceApp = await InheritanceApp.create(connection);
+    const inheritanceApp = await InheritanceApp.create(connection);
 
-  setTimeout(async () => {
-    //console.log('aborting');
-    //await inheritanceApp.abort();
-    //console.log('aborted');
-  }, 3000);
+    setTimeout(async () => {
+        //console.log('aborting');
+        //await inheritanceApp.abort();
+        //console.log('aborted');
+    }, 3000);
 
-  //console.log(walletList[1].id.join(','));
-  const walletId = walletList[1].id;
+    //console.log(walletList[1].id.join(','));
+    const walletId = walletList[1].id;
 
-  //await inheritanceApp.authWallet({
-  //  challenge: walletId,
-  //  walletId: walletId,
-  //  withPublicKey: true,
-  //  type: 'wallet-based',
-  //});
+    //await inheritanceApp.authWallet({
+    //  challenge: walletId,
+    //  walletId: walletId,
+    //  withPublicKey: true,
+    //  type: 'wallet-based',
+    //});
 
-  await inheritanceApp.testSessionStart();
+    await inheritanceApp.testSessionStart();
 
-  const thing = await inheritanceApp.encryptMessagesWithPin({
-    walletId: walletId,
-    messages: [
-      {
-        value: 'test message',
-        verifyOnDevice: false,
-      },
-      {
-        value:
-          'Lorem ipsum dolor sit amet, -=test "device" consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        verifyOnDevice: true,
-      },
-    ],
-  });
+    const thing = await inheritanceApp.encryptMessagesWithPin({
+        walletId: walletId,
+        messages: {
+            2: {
+                value: 'test value',
+                verifyOnDevice: true,
+            },
+        },
+    });
 
-  const output = await inheritanceApp.decryptMessagesWithPin({
-    walletId: walletId,
-    encryptedData: thing.encryptedPacket,
-  });
+    const output = await inheritanceApp.decryptMessagesWithPin({
+        walletId: walletId,
+        encryptedData: thing.encryptedPacket,
+    });
 
-  await inheritanceApp.testSessionStop();
+    await inheritanceApp.testSessionStop();
 
-  console.log(JSON.stringify(output));
+    console.log(JSON.stringify(output));
 };
 
 run();
