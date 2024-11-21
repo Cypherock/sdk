6a1c7c065a1ca0f59a57875d71d05efa9e4caa37
TejasvOnly <tejasvonly@gmail.com>
2024-11-21 01:40:45 -0800

    chore: remove obsolete tests

--- packages/core/tests/commands/__helpers__/index.ts
+++
@@ -1,7 +0,0 @@
-import { Msg } from '../../../src/encoders/proto/generated/core';
-
-export * from './mocks';
-
-export function msgToUint8Array(queryData: Msg): Uint8Array {
-  return Uint8Array.from(Msg.encode(Msg.create(queryData)).finish());
-}
