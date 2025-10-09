1f192bc20234b0edfd5aadd9d765531b560b344d
prashantrahul141 <prashantrahul141@protonmail.com>
2025-10-09 16:18:26 +0530

    feat: add node ordering
    Nodes are ordered in way that a node with children never comes before
    that children. Basically in a tree like structure, the leaves are
    ordered first, then branches then root.
    
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- packages/app-canton/src/operations/signTxn/index.ts
+++ packages/app-canton/src/operations/signTxn/index.ts
@@ -99,9 +99,63 @@ export const signTxn = async (
     assertOrThrowInvalidResult(txnNodeSeedAccepted);
   }
 
-  for (const node of prepareTransaction.transaction.nodes) {
+  // mapped node ids with their node objects
+  // nodeId  => Node { nodeId, ... }
+  const getNodesById = Object.fromEntries(
+    prepareTransaction.transaction.nodes.map(n => [n.nodeId, n]),
+  );
+
+  // This contains a map of nodes with their children
+  // parentid => [ children1, children2 ]
+  const nodesWithChildren: Record<string, string[]> = {};
+
+  for (const individualNode of prepareTransaction.transaction.nodes) {
+    if (
+      individualNode.versionedNode.oneofKind === 'v1' &&
+      individualNode.versionedNode.v1.nodeType.oneofKind
+    ) {
+      const v1Node = individualNode.versionedNode.v1;
+      // only exercise and rollback nodes has children
+      if (v1Node.nodeType.oneofKind === 'exercise') {
+        const exerciseNode = v1Node.nodeType.exercise;
+        nodesWithChildren[individualNode.nodeId] = exerciseNode.children;
+      } else if (v1Node.nodeType.oneofKind === 'rollback') {
+        const rollbackNode = v1Node.nodeType.rollback;
+        nodesWithChildren[individualNode.nodeId] = rollbackNode.children;
+      } else {
+        nodesWithChildren[individualNode.nodeId] = [];
+      }
+    }
+  }
+
+  // nodes which are already explored
+  const visitedNodes = new Set();
+  // this contains node ids in order they are safe to send
+  const orderedNodeIds: string[] = [];
+
+  const visitNode = (nodeId: string) => {
+    if (visitedNodes.has(nodeId)) {
+      return;
+    }
+    visitedNodes.add(nodeId);
+
+    for (const childrenId of nodesWithChildren[nodeId]) {
+      visitNode(childrenId);
+    }
+    orderedNodeIds.push(nodeId);
+  };
+
+  // start visiting nodes
+  for (const n of prepareTransaction.transaction.nodes) {
+    visitNode(n.nodeId);
+  }
+
+  for (const nodeId of orderedNodeIds) {
+    // DamlTransaction_Node.encode(node);
     // TODO: serialize node
-    console.log('node', node);
+    const node = getNodesById[nodeId];
+    console.log('nodeId : %d : %s', nodeId, node);
+
     const serializedNode = new Uint8Array();
     await helper.sendInChunks(serializedNode, 'txnNode', 'txnNodeAccepted');
   }
