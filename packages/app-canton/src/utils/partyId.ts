2aed2e0638339f6244efc6f2b3b6e94925be2b40
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2025-10-19 16:40:11 +0530

    fix: custom derive canton party id

--- packages/app-canton/src/utils/partyId.ts
+++ packages/app-canton/src/utils/partyId.ts
@@ -1,5 +1,4 @@
-import { sha256 } from '@cypherock/sdk-utils';
-import { getCantonLib } from './cantonLib';
+import { hexToUint8Array, sha256, uint8ArrayToHex } from '@cypherock/sdk-utils';
 
 // Ref:
 // https://docs.digitalasset.com/integrate/devnet/party-management/index.html#choosing-a-party-hint
@@ -12,17 +11,38 @@ import { getCantonLib } from './cantonLib';
 // https://github.com/hyperledger-labs/splice-wallet-kernel/blob/main/core/ledger-client/src/topology-write-service.ts#L143
 // Ref:
 // https://github.com/hyperledger-labs/splice-wallet-kernel/blob/main/core/ledger-client/src/topology-write-service.ts#L62
-// fingerprint = 0x1220 + sha256(HASH_PURPOSE + public_key)
+// fingerprint = "1220" + sha256(HASH_PURPOSE + public_key)
+const FINGERPRINT_HASH_PURPOSE = 12;
+const CANTON_HASH_PREFIX = Uint8Array.from([0x12, 0x20]);
+
+export const prefixedInt = (value: number, bytes: Uint8Array) => {
+  const buffer = Buffer.alloc(4 + bytes.length);
+  buffer.writeUInt32BE(value, 0);
+  Buffer.from(bytes).copy(buffer, 4);
+  return buffer;
+};
+
+export const computeSha256CantonHash = async (
+  purpose: number,
+  bytes: Uint8Array,
+) => {
+  const hashInput = prefixedInt(purpose, bytes);
+  const hash = await sha256(hashInput);
+
+  return uint8ArrayToHex(Uint8Array.from([...CANTON_HASH_PREFIX, ...hash]));
+};
+
+export const deriveFingerprintFromPublicKey = (publicKey: Uint8Array) =>
+  computeSha256CantonHash(FINGERPRINT_HASH_PURPOSE, publicKey);
+
+// deterministically derive party hint from fingerprint
+// party_hint = sha256_hash(fingerprint)[:5]
+export const derivePartyHintFromFingerprint = async (fingerprint: string) =>
+  uint8ArrayToHex((await sha256(hexToUint8Array(fingerprint))).slice(0, 5));
+
 export const derivePartyId = async (publicKey: Uint8Array) => {
-  const { TopologyController } = getCantonLib().CantonWalletSdk;
+  const fingerprint = await deriveFingerprintFromPublicKey(publicKey);
+  const partyHint = await derivePartyHintFromFingerprint(fingerprint);
 
-  const fingerprint = TopologyController.createFingerprintFromPublicKey(
-    Buffer.from(publicKey).toString('base64'),
-  );
-
-  const partyId = Buffer.from(
-    (await sha256(Buffer.from(fingerprint, 'hex'))).slice(0, 5),
-  ).toString('hex');
-
-  return `${partyId}::${fingerprint}`;
+  return `${partyHint}::${fingerprint}`;
 };
