0731891685499aaac3639cba719170af97a6303d
Keyur Vaitha <keyurvaitha@Keyurs-MacBook-Air.local>
2025-10-24 03:10:55 +0530

    feat: add sia operations

---
+++ packages/app-sia/src/operations/runGetPublicKeys/index.ts
@@ -0,0 +1,44 @@
+import { uint8ArrayToHex } from '@cypherock/sdk-utils';
+import { OperationHelper, assertOrThrowInvalidResult } from '../../utils';
+
+import { IGetPublicKeysParams, GetPublicKeysEvent } from './types';
+
+export * from './types';
+
+export const runGetPublicKeysOnDevice = async (
+  helper:
+    | OperationHelper<'getPublicKeys', 'getPublicKeys'>
+    | OperationHelper<'getUserVerifiedPublicKey', 'getUserVerifiedPublicKey'>,
+  params: IGetPublicKeysParams,
+  forceStatusUpdate: (flowStatus: number) => void,
+) => {
+  await helper.sendQuery({
+    initiate: {
+      walletId: params.walletId,
+      derivationPaths: params.derivationPaths,
+    },
+  });
+
+  let publicKeys: Uint8Array[] = [];
+  let addresses: string[] = [];
+  const hasMore = () => publicKeys.length !== params.derivationPaths.length;
+  do {
+    const result = await helper.waitForResult();
+    assertOrThrowInvalidResult(result.result);
+    publicKeys = [...publicKeys, ...result.result.publicKeys];
+    addresses = [...addresses, ...result.result.addresses];
+    forceStatusUpdate(GetPublicKeysEvent.PIN_CARD);
+    if (hasMore()) {
+      await helper.sendQuery({
+        fetchNext: {},
+      });
+    }
+  } while (hasMore());
+
+  forceStatusUpdate(GetPublicKeysEvent.VERIFY);
+
+  return {
+    publicKeys: publicKeys.map(key => uint8ArrayToHex(key)),
+    addresses,
+  };
+};
