77414220c95dc1077cef214b649539d0a80407c5
Prashant Rahul <prashantrahul141@protonmail.com>
2026-01-23 15:34:15 +0530

    test: add cardano sdk create test case
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

---
+++ packages/app-cardano/tests/01.create/index.ts
@@ -0,0 +1,25 @@
+import { MockDeviceConnection } from '@cypherock/sdk-interfaces';
+import { afterEach, beforeEach, describe, expect, test } from '@jest/globals';
+
+import * as sdkMocks from '../../src/__mocks__/sdk';
+import { CardanoApp } from '../../src';
+
+describe('CardanoApp.create', () => {
+  let connection: MockDeviceConnection;
+
+  beforeEach(async () => {
+    connection = await MockDeviceConnection.create();
+    sdkMocks.create.mockClear();
+  });
+
+  afterEach(async () => {
+    await connection.destroy();
+  });
+
+  test('should be able to create cardano app instance', async () => {
+    await CardanoApp.create(connection);
+
+    expect(sdkMocks.create).toHaveBeenCalledTimes(1);
+    expect(sdkMocks.create.mock.lastCall).toContainEqual(connection);
+  });
+});
